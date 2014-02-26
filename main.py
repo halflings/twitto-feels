c70f005e1e7f81811927baa4fac9a17b22bd5a74
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-26 21:24:59 +0100

    No features added, just some python cuz I'm tired of studying

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import sys
 import tweepy
 import json
 
 import config
 from tweet import Tweet
 
-auth1 = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth1.set_access_token(config.access_token_key, config.access_token_secret)
+auth_ = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth_.set_access_token(config.access_token_key, config.access_token_secret)
 
 class StreamListener(tweepy.StreamListener):
     def on_error(self, status_code):
-        print 'Error: ' + repr(status_code)
+        print >> sys.stderr, 'Error:', status_code
         return False
 
-    def on_data(self, data):
-        d = json.loads(data)
-        tweet = Tweet(d)
+    def on_data(self, raw_data):
+        data = json.loads(raw_data)
+        tweet = Tweet(data)
+        self.on_tweet(tweet)
+
+    def on_tweet(self, tweet):
+        """
+        Hanlder called when a new tweet is streamed, taking the associated
+        Tweet object.
+        """
         print tweet
-        pass
 
-l = StreamListener()
-streamer = tweepy.Stream(auth=auth1, listener=l)
+if __name__ == '__main__':
+    listener = StreamListener()
+    streamer = tweepy.Stream(auth=auth_, listener=listener)
 
-tags = ['happy']
-streamer.filter(track=tags)
+    tags = ['happy']
+    streamer.filter(track=tags)
--- tweet.py
+++ tweet.py
@@ -1,10 +1,17 @@
 def preprocess_text(text):
-    text = text.encode('utf-8').strip().lower()
-    return text
+    """
+    Prepare a string for syntaxic treatment (encode to utf-8, remove
+    leading/trailing whitespace, etc...).
+    """
+    return text.encode('utf-8').strip(' \t\r\n').lower()
 
 class Tweet(object):
+    """
+    Class handling object-oriented formatting of data received through
+    Twitter's API. Raw data is accessible through the *raw_data* member.
+    """
     def __init__(self, data):
-        self.data = data
+        self.raw_data = data
         self.user = preprocess_text(data['user']['screen_name'])
         self.text = preprocess_text(data['text'])
 
