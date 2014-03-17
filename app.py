ff9c82207190844368aa1042bd522323fdd4e28f
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-18 00:52:40 +0100

    More REST API fixes, tweets should be read-only BTW

--- app.py
+++ app.py
@@ -20,20 +20,15 @@ def mongo_get_or_abort(document_cls, pk, *args, **kwargs):
     except document_cls.DoesNotExist:
         abort(404)
 
+topic_public_fields = ('name', 'tags')
 class TopicResource(Resource):
     def get(self, topic_id):
         return mongo_to_dict(mongo_get_or_abort(Topic, topic_id))
 
-    def post(self):
-        topic = Topic(name=request.form['name'], tags=request.form['tags'])
-        topic.save()
-        return mongo_to_dict(topic)
-
     def put(self, topic_id):
         topic = mongo_get_or_abort(Topic, topic_id)
         topic = Topic.objects.get(pk=topic_id)
-        topic.name = request.form['name']
-        topic.tags = request.form['tags']
+        topic.update(**{ x: request.form[x] for x in topic_public_fields })
         topic.save()
         return mongo_to_dict(topic)
 
@@ -45,7 +40,21 @@ api.add_resource(TopicResource, '/topics/<topic_id>')
 class TopicsResource(Resource):
     def get(self):
         return mongo_to_dict(Topic.objects)
+
+    def post(self):
+        topic = Topic(**{ x: request.form[x] for x in topic_public_fields })
+        topic.save()
+        return mongo_to_dict(topic)
 api.add_resource(TopicsResource, '/topics')
+
+class TweetResource(Resource):
+    def get(self, tweet_id):
+        return mongo_to_dict(mongo_get_or_abort(Tweet, tweet_id))
+
+    def delete(self, tweet_id):
+        tweet = mongo_get_or_abort(Tweet, tweet_id)
+        tweet.delete()
+api.add_resource(TweetResource, '/tweets/<tweet_id>')
 
 class TweetsResource(Resource):
     def get(self):
