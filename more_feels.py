bc3298460c1d81beba00d78e98b67247b77da39a
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-22 14:40:15 +0100

    Fixed more_feels

--- more_feels.py
+++ more_feels.py
@@ -10,21 +10,26 @@ class TweetPolarity(Document):
     tweet = fields.ReferenceField(Tweet, required=True)
     polarity = fields.FloatField(required=True)
 
-def naive_tweet_polarity(tweet):
+def naive_tweet_polarity(tweet, save=False):
     blob = TextBlob(tweet.status)
     polarities = [x.sentiment.polarity for x in blob.sentences]
-    if not polarities:
-        return 0
-    return sum(polarities) / len(polarities)
+    tweet_polarity = TweetPolarity()
+    tweet_polarity.tweet = tweet
+    tweet_polarity.polarity = sum(polarities) / len(polarities) \
+            if polarities else 0
+    if save:
+        tweet_polarity.save()
+    return tweet_polarity
 
 if __name__ == '__main__':
     TweetPolarity.objects.delete()
-
-    #limit = 5
-    tweets = Tweet.objects#[:limit]
-    print 'Computations started!'
-    for i, tweet in enumerate(tweets):
-        print 'Progress: %s/%s' % (i, len(tweets))
-        tweet_polarity = TweetPolarity(tweet=tweet,
-                polarity=naive_tweet_polarity(tweet))
-        tweet_polarity.save()
+    nbtweets = len(Tweet.objects)
+    for i, tweet in enumerate(Tweet.objects):
+        print 'Progress: %s/%s' % (i, nbtweets)
+        try:
+            tweet_polarity = naive_tweet_polarity(tweet)
+            tweet_polarity.save()
+        except ValueError:
+            print 'Failed:', tweet.tweet_id
+        else:
+            tweet_polarity.save()
